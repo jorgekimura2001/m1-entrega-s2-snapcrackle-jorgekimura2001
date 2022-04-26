fb781b85d1b1690a643c75e07e2fbea19484de20
Jorge Kimura <kimurajorge2001@gmail.com>
2022-04-26 16:14:43 -0300

    Arquivos

---
+++ snapCrackle.js
@@ -0,0 +1,27 @@
+// se o número for igual a impar, ou seja, maxValue % 2 !== 0, concatenar Snap
+// mas se o número for múltiplo de 5, ou seja, maxValue % 5 == 0, concatenar Crackle
+// e se for igual a impar e múltiplo de 5, maxValue % 2 !== 0 && maxValue % 5 == 0, concatenar SnapCrackle
+// caso não seja igual a impar e multiplo de 5, maxValeu % 2 == 0 && maxValeu % 5 !== 0, concatenar maxValeu[i]
+
+function snapCrackle(maxValue) {
+
+    let result = []
+    for(let i = 1; i <= maxValue; i++) {
+        if (i % 2 !== 0 && i % 5 == 0) { // fluxo do javascript de cima à baixo 
+            result.push("SnapCrackle")   // cuidar para que as condicionais não interfiram uma na outra
+            continue
+        }
+        if (i % 2 !== 0) { 
+            result.push("Snap")
+            continue
+        }
+        if (i % 5 == 0) { 
+            result.push("Crackle")
+            continue
+        }
+        if(i % 2 === 0){
+            result.push(i)
+        }
+    }
+    return result.join(', ')
+}
